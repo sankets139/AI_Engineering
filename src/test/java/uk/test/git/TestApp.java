89f509e3a78f4affa2b204c89411be82cb53fbcc
sankets139 <110047973+sankets139@users.noreply.github.com>
2022-11-15 15:48:43 +0000

    Jira 123 - Committing the project for first time

---
+++ src/test/java/uk/test/git/TestApp.java
@@ -0,0 +1,11 @@
+package uk.test.git;
+
+import org.testng.annotations.Test;
+
+public class TestApp {
+
+    @Test
+    public void testApp(){
+        System.out.println("Running testApp");
+    }
+}
