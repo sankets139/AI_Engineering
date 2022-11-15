9edf014201db8519b5c6a3af18072c36ca4bf64b
sankets139 <110047973+sankets139@users.noreply.github.com>
2022-11-15 16:05:37 +0000

    Jira 123 - Committing the project for first time

--- src/main/java/uk/demo/git/Application.java
+++ src/main/java/uk/demo/git/Application.java
@@ -3,5 +3,6 @@ package uk.demo.git;
 public class Application {
     public static void main(String[] args) {
         System.out.println("Git is cool");
+        System.out.println("Created repo in GIT HUB");
     }
 }
